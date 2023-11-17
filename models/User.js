e42d2a669a989d95c6fecdf02e2371f9828d5b5e
김영완 <Kim-Young-Wan@MacBook-Air.local>
2023-11-17 15:20:27 +0900

    처음 저장소에 올림

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true, //이메일 빈칸 붙이기 
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    Image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
